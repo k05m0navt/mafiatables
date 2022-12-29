da63977dc17545679e451cd0ed00934258a7d65f
Daniil Gubaydullin <43779110+k05m0navt@users.noreply.github.com>
2022-12-29 06:53:52 +0300

    Set up react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/router";
 
 type AppProps = {};
 
 const App: FC<AppProps> = () => {
-  return <div className="text-red-500">App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
